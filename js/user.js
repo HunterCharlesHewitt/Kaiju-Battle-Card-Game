6fe85d60612c72d9197ff779ab6d2e1fb134f701
Hunter Hewitt <hunterchewitt@hotmail.com>
2021-04-24 02:22:01 -0500

    removed unecessary js file

--- js/user.js
+++
@@ -1,8 +0,0 @@
-function() {
-    $('form#username').submit(function(event) {
-        console.log("here")
-        socket.nickname = $('#username_data').val()
-        socket.emit('username_event', {data: $('#username_data').val()});
-        return false;
-    });
-}
